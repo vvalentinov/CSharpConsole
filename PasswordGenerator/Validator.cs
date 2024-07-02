ee0917f7c1e3cf4f927c3ecf22b723e1d8481bc6
Valentin <valentinvalentinov027@gmail.com>
2024-07-02 21:58:23 +0300

    Create initial Validator class
    Create initial Validator class with logic for validating the user input for password length

---
+++ PasswordGenerator/Validator.cs
@@ -0,0 +1,23 @@
+﻿namespace PasswordGenerator
+{
+    using static PasswordGenerator.ConsolePrinter;
+
+    public static class Validator
+    {
+        public static int ValidateLength(string input)
+        {
+            bool isNumber = int.TryParse(input, out int length);
+
+            while (!isNumber || length < 5 || length > 100)
+            {
+                Console.WriteLine("Error! Try, again!");
+
+                PrintChooseLengthMessage();
+
+                isNumber = int.TryParse(Console.ReadLine(), out length);
+            }
+
+            return length;
+        }
+    }
+}
