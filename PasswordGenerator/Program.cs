baec07b0603c145d2b7287b4f90b89e9ed73fc28
Valentin <valentinvalentinov027@gmail.com>
2024-07-02 22:01:35 +0300

    Add logic for initial console interaction

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static PasswordGenerator.Validator;
+using static PasswordGenerator.Generator;
+using static PasswordGenerator.ConsolePrinter;
+using static PasswordGenerator.CharactersList;
+
+PrintChooseLengthMessage();
+
+int passLength = ValidateLength(Console.ReadLine() ?? string.Empty);
+
+PrintCharacterOptions(Characters);
+
+PrintChooseOptionsMessage();
+
+string inputCharactersOptions = ValidateInputOptions(Console.ReadLine() ?? string.Empty);
+
+string characters = GenerateCharacters(inputCharactersOptions, Characters);
+
+string password = GeneratePassword(characters, passLength);
+
+Console.WriteLine(new string('=', 100));
+Console.WriteLine($"Your password: {password}");
+
+var fileName = GenerateFile(password);
+
+Console.WriteLine($"A file with name: {fileName} was created on Desktop!");
