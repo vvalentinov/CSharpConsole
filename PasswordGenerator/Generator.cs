adb206ba8f6a0ff151c3cda63050d50b85443682
Valentin <valentinvalentinov027@gmail.com>
2024-07-02 21:53:46 +0300

    Create initial Generator class

---
+++ PasswordGenerator/Generator.cs
@@ -0,0 +1,23 @@
+﻿namespace PasswordGenerator
+{
+    using System.Text;
+
+    public static class Generator
+    {
+        public static string GenerateCharacters(
+            string inputOptions,
+            IDictionary<char, string> charactersList)
+        {
+            string[] options = inputOptions.Split(' ');
+
+            var sb = new StringBuilder();
+
+            foreach (var option in options)
+            {
+                sb.Append(charactersList[Char.Parse(option)]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
