ef72e05319e9dcf84b3dbf0888c224e363866e04
Valentin <valentinvalentinov027@gmail.com>
2024-07-03 18:15:09 +0300

    Update Converter.cs
    Add other temperatures formulas

--- src/TemperatureConverter/Converter.cs
+++ src/TemperatureConverter/Converter.cs
@@ -3,5 +3,15 @@
     public static class Converter
     {
         public static double CelsiusToFahrenheit(int celsius) => (celsius * 1.8) + 32;
+
+        public static double CelsiusToKelvin(int celsius) => celsius + 273.15;
+
+        public static double FahrenheitToCelsius(int fahrenheit) => (fahrenheit - 32) * (5 / 9 * 1.0);
+
+        public static double FahrenheitToKelvin(int fahrenheit) => (fahrenheit - 32) * (5 / 9 * 1.0) + 273.15;
+
+        public static double KelvinToCelsius(int kelvin) => kelvin - 273.15;
+
+        public static double KelvinToFahrenheit(int kelvin) => (kelvin - 273.15) * 1.8 + 32;
     }
 }
