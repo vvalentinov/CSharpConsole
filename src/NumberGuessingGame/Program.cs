14c864d3c2459ae84ee1161fc7fc94670a437bc1
Valentin <valentinvalentinov027@gmail.com>
2024-07-06 18:52:10 +0300

    Create validation for the guessed number

--- src/NumberGuessingGame/Program.cs
+++ src/NumberGuessingGame/Program.cs
@@ -1,7 +1,9 @@
-﻿using static NumberGuessingGame.Validator;
+﻿using System.Security.Cryptography;
+using static NumberGuessingGame.Validator;
 
 Console.WriteLine("Welcome to the number guessing game!");
 Console.WriteLine("(Optional): You can choose lower and upper bound! (default: 0 - 100 000)");
+
 Console.Write("Lower bound: ");
 
 string lowerBoundInput = Console.ReadLine() ?? string.Empty;
@@ -14,7 +16,9 @@ if (!string.IsNullOrWhiteSpace(lowerBoundInput))
 
     while (!isLowerBoundValid)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(isLowerBoundValidMsg);
+        Console.ResetColor();
         Console.Write("Lower bound: ");
         lowerBoundInput = Console.ReadLine() ?? string.Empty;
         (isLowerBoundValid, isLowerBoundValidMsg) = ValidateLowerBound(lowerBoundInput);
@@ -22,3 +26,59 @@ if (!string.IsNullOrWhiteSpace(lowerBoundInput))
 
     lowerBound = int.Parse(lowerBoundInput);
 }
+
+Console.Write("Upper bound: ");
+
+string upperBoundInput = Console.ReadLine() ?? string.Empty;
+
+int upperBound = 100_000;
+
+if (!string.IsNullOrWhiteSpace(upperBoundInput))
+{
+    (bool isUpperBoundValid, string isUpperBoundValidMsg) = ValidateUpperBound(upperBoundInput, lowerBound);
+
+    while (!isUpperBoundValid)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(isUpperBoundValidMsg);
+        Console.ResetColor();
+        Console.Write("Upper bound: ");
+        upperBoundInput = Console.ReadLine() ?? string.Empty;
+        (isUpperBoundValid, isUpperBoundValidMsg) = ValidateUpperBound(upperBoundInput, lowerBound);
+    }
+
+    upperBound = int.Parse(upperBoundInput);
+}
+
+int randomNumber = RandomNumberGenerator.GetInt32(lowerBound, upperBound + 1);
+
+Console.WriteLine("The random number has been generated! Let the game begin!");
+Console.Write("Your guess: ");
+
+string guessedNumber = Console.ReadLine() ?? string.Empty;
+
+(bool isGuessedNumberValid, string isGuessedNumberValidMsg) = ValidateGuessedNumber(
+    guessedNumber,
+    lowerBound,
+    upperBound,
+    randomNumber);
+
+while (!isGuessedNumberValid)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(isGuessedNumberValidMsg);
+    Console.ResetColor();
+    Console.Write("Your guess: ");
+
+    guessedNumber = Console.ReadLine() ?? string.Empty;
+
+    (isGuessedNumberValid, isGuessedNumberValidMsg) = ValidateGuessedNumber(
+        guessedNumber,
+        lowerBound,
+        upperBound,
+        randomNumber);
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Hurray! Look's like the good guys always win at the end!");
+Console.ResetColor();
--- src/NumberGuessingGame/Validator.cs
+++ src/NumberGuessingGame/Validator.cs
@@ -2,7 +2,7 @@
 {
     using System.Numerics;
 
-    public class Validator
+    public static class Validator
     {
         public static (bool result, string message) ValidateLowerBound(string input)
         {
@@ -26,6 +26,69 @@
             if (lowerBoundParseResult > int.MaxValue)
             {
                 return (false, $"Error: The lower bound cannot be bigger than {int.MaxValue}! Try, again!");
+            }
+
+            return (true, "Valid");
+        }
+
+        public static (bool result, string message) ValidateUpperBound(string input, int lowerBound)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return (true, "Valid");
+            }
+
+            var isParsable = BigInteger.TryParse(input, out BigInteger upperBoundParseResult);
+
+            if (!isParsable)
+            {
+                return (false, "Error: You must enter a valid number! Try, again!");
+            }
+
+            if (upperBoundParseResult < 0)
+            {
+                return (false, "Error: The upper bound cannot be less than zero! Try, again!");
+            }
+
+            if (upperBoundParseResult > int.MaxValue)
+            {
+                return (false, $"Error: The upper bound cannot be bigger than {int.MaxValue}! Try, again!");
+            }
+
+            if (upperBoundParseResult <= lowerBound)
+            {
+                return (false, "Error: The upper bound cannot be less than or equal to the lower bound! Try, again!");
+            }
+
+            return (true, "Valid");
+        }
+
+        public static (bool result, string message) ValidateGuessedNumber(
+            string input,
+            int lowerBound,
+            int upperBound,
+            int randomNumber)
+        {
+            var isParsable = BigInteger.TryParse(input, out BigInteger number);
+
+            if (!isParsable)
+            {
+                return (false, "Error: You must enter a valid number! Try, again!");
+            }
+
+            if (number < lowerBound || number > upperBound)
+            {
+                return (false, $"Error: You must enter a number in the specified range: {lowerBound} - {upperBound}! Try, again!");
+            }
+
+            if (number < randomNumber)
+            {
+                return (false, "Error: You'll have to aim higher! Try, again!");
+            }
+
+            if (number > randomNumber)
+            {
+                return (false, "Error: You'll have to aim lower! Try, again!");
             }
 
             return (true, "Valid");
