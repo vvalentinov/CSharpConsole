70b971e5170394cc808cbfe95a5ca805063a9ae2
Valentin <valentinvalentinov027@gmail.com>
2024-07-06 18:01:49 +0300

    Create initial validator class
    Create initial validator class with  logic for validation for lower number bound

--- src/NumberGuessingGame/Program.cs
+++ src/NumberGuessingGame/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using static NumberGuessingGame.Validator;
+
+Console.WriteLine("Welcome to the number guessing game!");
+Console.WriteLine("(Optional): You can choose lower and upper bound! (default: 0 - 100 000)");
+Console.Write("Lower bound: ");
+
+string lowerBoundInput = Console.ReadLine() ?? string.Empty;
+
+int lowerBound = 0;
+
+if (!string.IsNullOrWhiteSpace(lowerBoundInput))
+{
+    (bool isLowerBoundValid, string isLowerBoundValidMsg) = ValidateLowerBound(lowerBoundInput);
+
+    while (!isLowerBoundValid)
+    {
+        Console.WriteLine(isLowerBoundValidMsg);
+        Console.Write("Lower bound: ");
+        lowerBoundInput = Console.ReadLine() ?? string.Empty;
+        (isLowerBoundValid, isLowerBoundValidMsg) = ValidateLowerBound(lowerBoundInput);
+    }
+
+    lowerBound = int.Parse(lowerBoundInput);
+}
---
+++ src/NumberGuessingGame/Validator.cs
@@ -0,0 +1,34 @@
+﻿namespace NumberGuessingGame
+{
+    using System.Numerics;
+
+    public class Validator
+    {
+        public static (bool result, string message) ValidateLowerBound(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return (true, "Valid");
+            }
+
+            var isParsable = BigInteger.TryParse(input, out BigInteger lowerBoundParseResult);
+
+            if (!isParsable)
+            {
+                return (false, "Error: You must enter a valid number! Try, again!");
+            }
+
+            if (lowerBoundParseResult < 0)
+            {
+                return (false, "Error: The lower bound cannot be less than zero! Try, again!");
+            }
+
+            if (lowerBoundParseResult > int.MaxValue)
+            {
+                return (false, $"Error: The lower bound cannot be bigger than {int.MaxValue}! Try, again!");
+            }
+
+            return (true, "Valid");
+        }
+    }
+}
