aa18c47e71f01aa7d9241d186f6b833122010551
Valentin <valentinvalentinov027@gmail.com>
2024-07-27 19:25:31 +0300

    Update Program.cs
    Listen for user input and analyze text

--- TextAnalyzer/Program.cs
+++ TextAnalyzer/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static TextAnalyzer.Errors;
+using static TextAnalyzer.Printer;
+using static TextAnalyzer.Analyzer;
+
+PrintWelcomeMessage();
+
+PrintTextInput();
+
+string? text = Console.ReadLine();
+
+while (string.IsNullOrWhiteSpace(text))
+{
+    DisplayError(EmptyTextError);
+    PrintTextInput();
+    text = Console.ReadLine();
+}
+
+AnalyzeText(text);
