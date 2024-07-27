484184268a34b8a991121d8a9a8362d1cbb6268c
Valentin <valentinvalentinov027@gmail.com>
2024-07-27 19:22:02 +0300

    Create initial errors constants helper class

---
+++ TextAnalyzer/Errors.cs
@@ -0,0 +1,11 @@
+﻿namespace TextAnalyzer
+{
+    public static class Errors
+    {
+        private const string Prefix = "Error: ";
+
+        public const string NoWordsError = $"{Prefix}Looks like no words were found in the text!";
+
+        public const string EmptyTextError = $"{Prefix}Looks like you didn't paste any text! Try, again!";
+    }
+}
