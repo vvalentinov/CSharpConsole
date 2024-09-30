679ad8ac968e242eb1d9599d74c7df712f4c2a76
Valentin <valentinvalentinov027@gmail.com>
2024-09-30 20:00:05 +0300

    Create method for creating water intake table

--- src/WaterIntake/Utils.cs
+++ src/WaterIntake/Utils.cs
@@ -1,5 +1,9 @@
 ﻿namespace WaterIntake
 {
+    using Microsoft.Data.Sqlite;
+
+    using static WaterIntake.DbCommands;
+
     public static class Utils
     {
         public static string GetDbConnectionString()
@@ -9,6 +13,23 @@
                 @"..\..\..\HabitTracker.db");
 
             return $"Data Source={dbPath}";
+        }
+
+        public static async Task CreateWaterIntakeTableAsync()
+        {
+            var dbConnection = GetDbConnectionString();
+
+            await using var connection = new SqliteConnection(dbConnection);
+
+            await connection.OpenAsync();
+
+            var createTableCommand = new SqliteCommand(
+                CreateDrinkingHabitTableCmd,
+                connection);
+
+            await createTableCommand.ExecuteNonQueryAsync();
+
+            await connection.CloseAsync();
         }
     }
 }
