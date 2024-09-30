b8c192347e13848348ec75f4811aa9e9bf753e4a
Valentin <valentinvalentinov027@gmail.com>
2024-09-30 20:01:06 +0300

    Create initial method for inserting a record

--- src/WaterIntake/Utils.cs
+++ src/WaterIntake/Utils.cs
@@ -1,7 +1,7 @@
 ﻿namespace WaterIntake
 {
     using Microsoft.Data.Sqlite;
-
+    using System.Globalization;
     using static WaterIntake.DbCommands;
 
     public static class Utils
@@ -30,6 +30,81 @@
             await createTableCommand.ExecuteNonQueryAsync();
 
             await connection.CloseAsync();
+        }
+
+        public static async Task InsertAsync()
+        {
+            string date = GetDateInput();
+
+            if (string.IsNullOrEmpty(date))
+            {
+                return;
+            }
+
+            var connectionString = GetDbConnectionString();
+
+            decimal litres = GetLitresInput();
+
+            using var connection = new SqliteConnection(connectionString);
+
+            await connection.OpenAsync();
+
+            string query = $"INSERT INTO DrinkingHabit(date, litres) VALUES('{date}', {litres})";
+
+            var tableCmd = new SqliteCommand(query, connection);
+
+            await tableCmd.ExecuteNonQueryAsync();
+
+            await connection.CloseAsync();
+        }
+
+        private static decimal GetLitresInput()
+        {
+            Console.WriteLine("Insert litres quantity.");
+
+            string numberInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(numberInput))
+            {
+                return 0;
+            }
+
+            bool isValidNumber = decimal.TryParse(numberInput, out decimal litres);
+            bool isValid = isValidNumber && litres > 0;
+
+            while (!isValid)
+            {
+                Console.WriteLine("Invalid number for litres! Must be a positive integer! Try again!");
+                isValidNumber = decimal.TryParse(numberInput, out litres);
+                isValid = isValidNumber && litres > 0;
+            }
+
+            return litres;
+        }
+
+        private static string GetDateInput()
+        {
+            Console.WriteLine("Please insert the date: (Format: dd-mm-yy).");
+
+            string dateInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(dateInput))
+            {
+                return string.Empty;
+            }
+
+            while (!DateTime.TryParseExact(
+                dateInput,
+                "dd-MM-yy",
+                new CultureInfo("en-US"),
+                DateTimeStyles.None,
+                out _))
+            {
+                Console.WriteLine("Invalid date. (Format: dd-mm-yy). Type 0 to return to main manu or try again:");
+                dateInput = Console.ReadLine();
+            }
+
+            return dateInput;
         }
     }
 }
