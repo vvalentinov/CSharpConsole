c15ea5cf2e6cbce64873ed442f4d68a978971f5b
Valentin <valentinvalentinov027@gmail.com>
2024-09-30 19:56:19 +0300

    Create Utils.cs
    Create initial Utils class with method for getting the db connection string

---
+++ src/WaterIntake/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace WaterIntake
+{
+    public static class Utils
+    {
+        public static string GetDbConnectionString()
+        {
+            string dbPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                @"..\..\..\HabitTracker.db");
+
+            return $"Data Source={dbPath}";
+        }
+    }
+}
