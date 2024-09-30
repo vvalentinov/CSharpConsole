ef54591127823e07de8d7f86a06f724cc71f4068
Valentin <valentinvalentinov027@gmail.com>
2024-09-30 19:55:18 +0300

    Create DbCommands.cs
    Create initial static class DbCommands

---
+++ src/WaterIntake/DbCommands.cs
@@ -0,0 +1,12 @@
+﻿namespace WaterIntake
+{
+    public static class DbCommands
+    {
+        public const string CreateDrinkingHabitTableCmd = @"
+            CREATE TABLE IF NOT EXISTS DrinkingHabit (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Date TEXT NOT NULL,
+                Litres REAL NOT NULL
+            )";
+    }
+}
