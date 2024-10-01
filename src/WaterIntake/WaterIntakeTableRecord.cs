958b698e28b753730a96238ec589cfee2c2ed4a8
Valentin <valentinvalentinov027@gmail.com>
2024-10-01 18:27:10 +0300

    Create WaterIntakeTableRecord.cs

---
+++ src/WaterIntake/WaterIntakeTableRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace WaterIntake
+{
+    public class WaterIntakeTableRecord
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Litres { get; set; }
+
+        public override string ToString()
+            => $"Id: {Id}, Date: {Date:dd-MMM-yyyy}, Litres: {Litres}";
+    }
+}
