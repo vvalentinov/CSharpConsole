fc49da0a96039ee20815e9754677f97d4e945a1c
Valentin <valentinvalentinov027@gmail.com>
2024-10-01 18:27:01 +0300

    Create DbConstants
    Create DbConstants class to hold constants related to the database such as table name or queries

---
+++ src/WaterIntake/DbConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace WaterIntake
+{
+    public static class DbConstants
+    {
+        public const string TableName = "DrinkingHabit";
+
+        public const string TableDateColumnFormat = "yyyy-MM-dd";
+
+        public const string CreateDrinkingHabitTableQuery = @$"
+            CREATE TABLE IF NOT EXISTS {TableName} (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Date TEXT NOT NULL,
+                Litres REAL NOT NULL
+            )";
+
+        public const string GetAllRecordsQuery = $"SELECT * FROM {TableName}";
+
+        public const string DeleteRecordQuery = $"DELETE FROM {TableName} WHERE Id = @RecordID";
+    }
+}
