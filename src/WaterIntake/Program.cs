bfad1717dcc8f8b8cf637257211b518de1406592
Valentin <valentinvalentinov027@gmail.com>
2024-10-01 18:28:20 +0300

    Update Program.cs
    Create initial start up functionality

--- src/WaterIntake/Program.cs
+++ src/WaterIntake/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static WaterIntake.WaterIntakeDataAccess;
+using static WaterIntake.ConsolePrinter;
+
+await CreateWaterIntakeTableAsync();
+
+bool closeApp = false;
+
+while (!closeApp)
+{
+    PrintMainMenu();
+    PrintYourChoiceMessage();
+
+    bool isInputValid = int.TryParse(Console.ReadLine(), out int command);
+
+    while (!isInputValid)
+    {
+        PrintInvalidMenuOptionError();
+        PrintMainMenu();
+        PrintYourChoiceMessage();
+        isInputValid = int.TryParse(Console.ReadLine(), out command);
+    }
+
+    switch (command)
+    {
+        case 0:
+            PrintGoodbyeMessage();
+            closeApp = true;
+            break;
+        case 1:
+            await GetAllRecordsAsync();
+            break;
+        case 2:
+            await InsertAsync();
+            break;
+        case 3:
+            await DeleteRecordAsync();
+            break;
+        case 4:
+            await UpdateRecordAsync();
+            break;
+        default:
+            PrintInvalidMenuOptionError();
+            break;
+    }
+}
