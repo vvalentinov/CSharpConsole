b28d8888b00ac5336acd05d1debee6cd95e641bd
Valentin <valentinvalentinov027@gmail.com>
2024-10-01 14:53:04 +0300

    Create ConsolePrinter.cs
    Create console printer class with initial method for printing the menu

---
+++ src/WaterIntake/ConsolePrinter.cs
@@ -0,0 +1,19 @@
+﻿namespace WaterIntake
+{
+    public static class ConsolePrinter
+    {
+        public static void PrintMainMenu()
+        {
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("Type 0 to close the application.");
+            Console.WriteLine("Type 1 to view all records.");
+            Console.WriteLine("Type 2 to insert a record.");
+            Console.WriteLine("Type 3 to delete a record.");
+            Console.WriteLine("Type 4 to update a record.");
+            Console.WriteLine($"{new string('-', 50)}{Environment.NewLine}");
+
+            Console.Write("Your choice: ");
+        }
+    }
+}
