fdc0d9e92b4e67594eab72d59b9c808549b1caf5
Valentin <valentinvalentinov027@gmail.com>
2024-10-01 18:25:47 +0300

    Create ConsoleReader.cs
    Create initial console reader class for getting the user input

---
+++ src/WaterIntake/ConsoleReader.cs
@@ -0,0 +1,117 @@
+﻿namespace WaterIntake
+{
+    using System.Globalization;
+
+    using static WaterIntake.ConsolePrinter;
+
+    public static class ConsoleReader
+    {
+        public static decimal GetLitresInput(bool isForUpdate = false)
+        {
+            string message = isForUpdate ?
+                "Insert litres quantity. (Type Enter to remain unchanged!)" :
+                "Insert litres quantity. Type 0 to abort and return to Menu!";
+
+            Console.WriteLine(message);
+            PrintLitresInputMessage();
+
+            string numberInput = Console.ReadLine();
+
+            if (isForUpdate && string.IsNullOrWhiteSpace(numberInput))
+            {
+                return 0;
+            }
+
+            if (!isForUpdate && numberInput == "0")
+            {
+                Console.WriteLine();
+                return 0;
+            }
+
+            bool isValid =
+                decimal.TryParse(numberInput, out decimal litres) &&
+                litres > 0;
+
+            while (!isValid)
+            {
+                PrintInvalidLitresInputError();
+                PrintLitresInputMessage();
+
+                numberInput = Console.ReadLine();
+
+                if (isForUpdate && string.IsNullOrWhiteSpace(numberInput))
+                {
+                    return 0;
+                }
+
+                if (!isForUpdate && numberInput == "0")
+                {
+                    Console.WriteLine();
+                    return 0;
+                }
+
+                isValid = decimal.TryParse(numberInput, out litres) && litres > 0;
+            }
+
+            return litres;
+        }
+
+        public static string GetDateInput(bool isForUpdate = false)
+        {
+            string message = isForUpdate ?
+                "Please insert the date: (Format: dd.MM.yyyy). (Type Enter to remain unchanged!)" :
+                "Please insert the date: (Format: dd.MM.yyyy). Type 0 to abort and return to Menu!";
+
+            Console.WriteLine(message);
+            PrintDateInputMessage();
+
+            string dateInput = Console.ReadLine();
+
+            if (isForUpdate && string.IsNullOrWhiteSpace(dateInput))
+            {
+                return "0";
+            }
+
+            if (!isForUpdate && dateInput == "0")
+            {
+                Console.WriteLine();
+                return "0";
+            }
+
+            bool isValidDate = DateTime.TryParseExact(
+                dateInput,
+                "dd.MM.yyyy",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out DateTime date);
+
+            while (!isValidDate)
+            {
+                PrintInvalidDateInputError();
+                PrintDateInputMessage();
+
+                dateInput = Console.ReadLine();
+
+                if (isForUpdate && string.IsNullOrWhiteSpace(dateInput))
+                {
+                    return "0";
+                }
+
+                if (!isForUpdate && dateInput == "0")
+                {
+                    Console.WriteLine();
+                    return "0";
+                }
+
+                isValidDate = DateTime.TryParseExact(
+                    dateInput,
+                    "dd.MM.yyyy",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out date);
+            }
+
+            return date.ToString("yyyy-MM-dd");
+        }
+    }
+}
