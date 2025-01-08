901f111604f23e3593d3f4ff5ec155ce0891fcb7
Valentin <valentinvalentinov027@gmail.com>
2025-01-08 18:35:34 +0200

    Create initial logic for prime number checker

--- src/PrimeChecker/Program.cs
+++ src/PrimeChecker/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+
+bool isPrime = true;
+
+if (number <= 1)
+{
+	isPrime = false;
+}
+else
+{
+	int limit = (int)Math.Sqrt(number);
+
+	for (int i = 2; i <= limit; i++)
+	{
+		if (number % i == 0)
+		{
+            isPrime = false;
+			break;
+        }
+	}
+}
+
+if (isPrime)
+{
+    Console.WriteLine("The entered number is a prime number! Goodbye!");
+}
+else
+{
+    Console.WriteLine("The entered number is not a prime number! Goodbye!");
+}
